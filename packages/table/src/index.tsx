dce4fb9d7e6b3e4a3ba1aab66067119825f78974
Gavin <876575488@qq.com>
2023-11-29 22:54:18 +0800

    pro-table去掉antd样式的引入，由项目自行引入

--- packages/table/src/index.tsx
+++ packages/table/src/index.tsx
@@ -1,5 +1,5 @@
 // 兼容代码-----------
-import 'antd/es/table/style';
+// import 'antd/es/table/style';
 //----------------------
 
 import { FieldIndexColumn, FieldStatus } from '@ant-design/pro-field';
